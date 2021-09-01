bb2e1e4198982f350ad6e222922fba60ba033b93
JosephRamayrat <joseph@ramayrat.com>
2021-09-01 15:33:15 -0700

    Project commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="josephramayrat/udacity"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacity --image=josephramayrat/udacity --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity 8000:80
